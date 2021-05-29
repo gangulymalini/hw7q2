076a87a2034c37b7794acff89731a40699302471
Malini Ganguly <gangulym@oregonstate.edu>
2021-05-29 15:21:31 -0700

    testcases

--- q2code.py
+++ q2code.py
@@ -1,3 +1,12 @@
-def leapyear():
-    teststring = ""
-    return(teststring)
+def leapyear(username):
+    if (username % 400) == 0 :
+        return("is a leap year")
+    else:
+        if (username % 100) == 0 :
+            return("is not a leap year")
+        else:
+            if (username % 4) == 0 :
+                return("is a leap year")
+            else:
+                return("is not a leap year")
+
--- q2test.py
+++ q2test.py
@@ -3,7 +3,9 @@ import q2code
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        self.assertEqual(q2code.leapyear(), 'heloo')
+        self.assertEqual(q2code.leapyear(300), "is not a leap year")
+    def test2(self):
+        self.assertEqual(q2code.leapyear(400), "is a leap year")
 
 if __name__ == '__main__':
     unittest.main()
